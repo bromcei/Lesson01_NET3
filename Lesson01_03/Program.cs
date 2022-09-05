644297737a00a05c30ab511516220ab3a4218027
tomas.ceida <tomceid@gmail.com>
2022-09-05 21:02:18 +0300

    add

---
+++ Lesson01_03/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson01_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"24 / 5 = {24m / 5m}");
+        }
+    }
+}
---
+++ Lesson01_04/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson01_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"-6 + 3 * 5 = {-6.0 + (3.0 * 5.0)}");
+            Console.WriteLine($"(5 + (-2)) + (20 / 10) = {(5 + (-2)) + (20m / 10m)}");
+
+        }
+    }
+}
