340425086b2271fd444a7b19bea2ad2a2eaac91e
tomas.ceida <tomceid@gmail.com>
2022-09-05 20:39:58 +0300

    Add project files.

---
+++ Lesson01_01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson01_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kokia tavo megstamiausia spalva?");
+            string spalva = Console.ReadLine();
+            Console.WriteLine($"MANO IRGI {spalva}");
+        }
+    }
+}
---
+++ Lesson01_02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson01_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"17 + 23 = {17 + 23}");
+        }
+    }
+}
